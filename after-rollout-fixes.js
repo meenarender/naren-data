7f73045a0df95fa780f30eeb9c5be08ef4163d2a
Narenda Kumar <Narendra@aapc.com>
2018-10-16 15:36:39 +0530

    add js file

---
+++ after-rollout-fixes.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+        var a = window.location.pathname; //  /medical-coding-education/conferences/regional/anaheim-2018/
+        //alert(a);
+    if (a == "/medical-coding-education/conferences/regional/anaheim-2018/registration.aspx") {
+       
+        $("#disneyland").parent().next(".well").children("p").append('- <strong class="red-txt">Sold Out</strong>');
+        $("#disneyland").closest(".col-md-8").next(".col-md-4").find(".disnyland-hotel-img").children("p").append('- <strong class="red-txt">Sold Out</strong>');
+        $("#disneyland").remove();
+        
+}
+
+
+});
